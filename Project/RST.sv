52fcc26d15927507999ee146a17e5207956faf27
LeLong356 <longhuynhc@gmail.com>
2025-09-19 12:53:19 +0700

    Update RST.sv

--- Project/RST.sv
+++ Project/RST.sv
@@ -1,1 +1,17 @@
+module RST(
+    input logic clk,
+    input logic [7:0] data_in,
+    input logic rst,
+    input logic load,
+    output logic [7:0] data_out
+);
+    always_ff @(posedge clk)begin
+        if(rst) begin 
+            data_out <= 8'b0;
+        end
+        else if(load) begin
+            data_out <= data_in;
+        end
+    end
 
+endmodule
