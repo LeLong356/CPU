7f46df9cb0ad64a99f05754b36b3332a92e12112
LeLong356 <longhuynhc@gmail.com>
2025-09-19 12:52:39 +0700

    Update MEM.sv

--- Project/MEM.sv
+++ Project/MEM.sv
@@ -1,1 +1,24 @@
+module MEM (
+    input  logic       clk,    
+    input  logic       en,     
+    input  logic       we,    
+    input  logic [4:0] addr,   
+    input  logic [7:0] din,    
+    output logic [7:0] dout    
+);
+
+    
+    logic [7:0] mem [0:31];
+
+    always_ff @(posedge clk) begin
+        if (en) begin
+            if (we) begin
+                mem[addr] <= din;
+            end else begin
+                dout <= mem[addr];
+            end
+        end
+    end
+
+endmodule
 
