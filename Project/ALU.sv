5249bae931066edf6d9fb066b1617cefeddae058
LeLong356 <longhuynhc@gmail.com>
2025-09-20 23:58:17 +0700

    Update ALU.sv

--- Project/ALU.sv
+++ Project/ALU.sv
@@ -4,12 +4,17 @@ module ALU (
     output logic [7:0] rd,
     output logic is_zero
 );
+    assign is_zero = ( rs1 == 0);
     logic [7:0] lut [0:7];
     always_comb begin
-        lut[2] = rs1 + rs2; // 010
-        lut[3] = rs1 & rs2; //011
-        lut[4] = rs1 ^ rs2; //100
-        rd = lut[opcode];
+        if (is_zero) begin
+            rd = 8'b0;
+        end
+        else begin 
+            lut[2] = rs1 + rs2; // 010
+            lut[3] = rs1 & rs2; //011
+            lut[4] = rs1 ^ rs2; //100
+            rd = lut[opcode];
+        end
     end
-    assign is_zero = ( rs1 == 0);
 endmodule
