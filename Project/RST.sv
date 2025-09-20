4dd92c3aa4d830492aac34aefd5f22b157218ee3
Minh-0405 <minh.du040506@hcmut.edu.vn>
2025-09-20 20:01:21 +0700

    Update RST.sv

--- Project/RST.sv
+++ Project/RST.sv
@@ -1,16 +1,18 @@
-module RST(
-    input logic clk,
-    input logic [7:0] data_in,
-    input logic rst,
-    input logic load,
-    output logic [7:0] data_out
+module Acc_reg (
+        input logic clk, rst,
+        input logic [7:0] data_mem, data_alu,
+        input logic load, control,
+        output logic [7:0] data_out
 );
     always_ff @(posedge clk)begin
-        if(rst) begin 
+        if(rst)
+        begin
             data_out <= 8'b0;
         end
-        else if(load) begin
-            data_out <= data_in;
+        else if(load)
+        begin
+            if(control) data_out <= data_mem ;
+            else data_out <= data_alu ;
         end
     end
 
