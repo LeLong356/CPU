6845afab325837c5c9d029a184e94595e7b54573
LeLong356 <longhuynhc@gmail.com>
2025-09-25 20:27:02 +0700

    Update CONTROL.sv

--- Project/CONTROL.sv
+++ Project/CONTROL.sv
@@ -17,23 +17,6 @@ module CONTROL (
 
     statetype_e state, nextstate;
 
-    always_comb begin
-    if (!halt) begin
-        case (state)
-            s0: nextstate = s1;
-            s1: nextstate = s2;
-            s2: nextstate = s3;
-            s3: nextstate = s4;
-            s4: nextstate = s1;
-            default: nextstate = s0;
-        endcase
-    end else begin
-        nextstate = state; // giữ nguyên khi halt
-    end
-end
-    
-
-    
     logic ACC_LOAD, ACC_MEM, STO, HALT, JMP, SKZ ;
     always_comb
     begin
@@ -51,6 +34,21 @@ end
     else
         state <= nextstate;
     end
+    
+    always_comb begin
+    if (!HALT) begin
+        case (state)
+            s0: nextstate = s1;
+            s1: nextstate = s2;
+            s2: nextstate = s3;
+            s3: nextstate = s4;
+            s4: nextstate = s1;
+            default: nextstate = s0;
+        endcase
+    end else begin
+        nextstate = state; // giữ nguyên khi halt
+    end
+end
     
     always_comb begin
     if (rst) begin
